72a9541a330535c2b0976fbb8504dbc4ace72b99
Amir Dillard <dillard24a@ncssm.edu>
2025-09-21 20:32:47 -0400

    changed to correct arguments for fuel generator

--- src/main/java/edu/unc/comp301/a02mothership/Main.java
+++ src/main/java/edu/unc/comp301/a02mothership/Main.java
@@ -2,7 +2,7 @@ package edu.unc.comp301.a02mothership;
 
 public class Main {
     public void main(String[] args){
-        FuelGenerator powerGenerator = new FuelGenerator("generator", 20);
+        FuelGenerator powerGenerator = new FuelGenerator(20);
         ThrusterModule thrusterModule = new ThrusterModule("thruster");
         double[] experimentDouble = {2.0, 3.0};
         ExperimentModule experimentModule = new ExperimentModule("experi", experimentDouble);
